f70359d1b56a6c9c2aa04e5302d52188b350b052
SubhaNITS150 <subhajyotidey2910@gmail.com>
2025-12-17 15:32:24 +0530

    Added cors policy

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/SubhaNITS150/dob-calculator/internal/repository"
 	"github.com/SubhaNITS150/dob-calculator/internal/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -24,6 +25,11 @@ func main() {
 	handler := handler.NewUserHandler(repo)
 
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://127.0.0.1:3000",
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 	routes.Setup(app, handler)
 
 	log.Println("Server listening on port 8090")
